22e25c93175a53a6b4355b93fe42c46b871fb083
CSharpRU <c_sharp@mail.ru>
2020-11-06 11:58:56 +0700

    add custom input

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,17 @@ REPOSITORY_NAME=$(basename "${GITHUB_REPOSITORY}")
 [[ -z ${INPUT_PROJECTNAME} ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
 [[ -z ${INPUT_PROJECTVERSION} ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
 
-echo ${INPUT_CUSTOM}
+echo "sonar-scanner \
+	-Dsonar.host.url=${INPUT_HOST} \
+	-Dsonar.projectKey=${SONAR_PROJECTKEY} \
+	-Dsonar.projectName=${SONAR_PROJECTNAME} \
+	-Dsonar.projectVersion=${SONAR_PROJECTVERSION} \
+	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+	-Dsonar.login=${INPUT_LOGIN} \
+	-Dsonar.password=${INPUT_PASSWORD} \
+	-Dsonar.sources=. \
+	-Dsonar.sourceEncoding=UTF-8 \
+	${INPUT_CUSTOM}"
 
 sonar-scanner \
 	-Dsonar.host.url=${INPUT_HOST} \
